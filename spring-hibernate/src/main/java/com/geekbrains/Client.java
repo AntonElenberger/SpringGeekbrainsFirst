2b54004ae92a4688b9be5f1d0d80518cdd12f630
AntonElenberger <antonelenberger@gmail.com>
2019-02-18 21:06:35 +0500

    L3 Homework

---
+++ spring-hibernate/src/main/java/com/geekbrains/Client.java
@@ -0,0 +1,56 @@
+package com.geekbrains;
+
+
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "client")
+    private List<Product> products;
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Client id:" + id + " name:" + name;
+    }
+
+}
---
+++ spring-hibernate/src/main/java/com/geekbrains/Product.java
@@ -0,0 +1,62 @@
+package com.geekbrains;
+
+import javax.persistence.*;
+
+@Entity()
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "cost")
+    private Double cost;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Product() {
+    }
+
+    @Override
+    public String toString() {
+        return "Product id:" + id + " name:" + brand + " and cost:" + cost;
+    }
+}
